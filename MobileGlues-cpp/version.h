34a77b4475a2d167b79899df6e936d3d22cd6ed2
VinzzLK <kevinalt928@gmail.com>
2026-04-01 19:08:14 +0700

    VinzzRenderer v1.0.0

---
+++ MobileGlues-cpp/version.h
@@ -0,0 +1,31 @@
+// VinzzRender - version.h
+// Fork of MobileGlues by MobileGL-Dev (LGPL-2.1)
+// Modified by Vinzz - VinzzRender
+// SPDX-License-Identifier: LGPL-2.1-only
+
+#ifndef MOBILEGLUES_VERSION_H
+
+#define VERSION_DEVELOPMENT 0
+#define VERSION_ALPHA 1
+#define VERSION_BETA 2
+#define VERSION_RC 3
+#define VERSION_RELEASE 10
+
+#define MAJOR 1
+#define MINOR 0
+#define REVISION 0
+#define PATCH 0
+
+#define VERSION_TYPE VERSION_RELEASE
+#define VERSION_SUFFIX ""
+
+// VinzzRender identity
+#define VINZZRENDER 1
+#define VINZZRENDER_VERSION 100
+#define VINZZRENDER_VERSION_STRING "VinzzRender v1.0.0"
+
+// Target hardware
+#define VINZZ_TARGET_ADRENO_650 1
+
+#define MOBILEGLUES_VERSION_H
+#endif
